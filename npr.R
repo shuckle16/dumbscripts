f6b51398e8eb8719b2d63ced9846076054be4404
shuckle16 <shuckle16@users.noreply.github.com>
2015-10-22 01:14:23 -0400

    some articles don't have full text available

--- npr.R
+++ npr.R
@@ -1,17 +1,23 @@
 library(rvest)
 library(rjson)
-datez <- seq(as.Date("2015/6/16"), as.Date("2015/6/21"), "days")
+datez <- as.character(seq(as.Date("2015/6/16"), as.Date("2015/7/15"), "days"))
 articlez <- character(300)
+idz <- numeric(300)
+pubdatez <- character(300)
 iterator <- 0
 
 for (d in datez) {
-  api_link <- paste("http://api.npr.org/query?startDate=",datez[1],"&searchTerm=donald%20trump&dateType=story&sort=dateAsc&numResults=50&output=JSON&apiKey=MDIwNDU5NzMyMDE0NDIwODE2NzNkOWE0OA001",sep="")
+  api_link <- paste("http://api.npr.org/query?startDate=",d,"&searchTerm=donald%20trump&dateType=story&sort=dateAsc&numResults=50&output=JSON&apiKey=MDIwNDU5NzMyMDE0NDIwODE2NzNkOWE0OA001",sep="")
   
   npr <- fromJSON(read_html(api_link) %>% html_text())$list$story
   
   for (j in 1:length(npr)) { 
-    iterator <- iterator + 1
-    articlez[iterator]<-npr[[j]]$fullText
+    if (!(npr[[j]]$id %in% idz)) {
+      iterator <- iterator + 1
+      articlez[iterator]<-npr[[j]]$fullText
+      idz[iterator] <- npr[[j]]$id
+      pubdatez[iterator] <- npr[[j]]$storyDate
+    }
   }
 }
 
