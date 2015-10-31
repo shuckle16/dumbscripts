1b3ebbc4371dc1e98628145194f4534a580829aa
shuckle16 <shuckle16@users.noreply.github.com>
2015-10-30 23:31:38 -0400

    Update clean_npr_articles.R

--- clean_npr_articles.R
+++ clean_npr_articles.R
@@ -4,8 +4,8 @@ library(stringr)
 library(tm)
 
 articlez <- str_replace(string = articlez,
-            pattern = "Copyright 2015 NPR. To see more, visit http://www.npr.org/.",
-            replacement = "")
+                        pattern = "Copyright 2015 NPR. To see more, visit http://www.npr.org/.",
+                        replacement = "")
 
 articlez <- str_to_lower(articlez)
 articlez <- removeWords(articlez,stopwords())
