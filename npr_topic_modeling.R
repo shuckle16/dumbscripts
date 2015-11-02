5acbaab4b4692bb6ba9210c062845996cb8732a3
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-02 15:54:06 -0500

    Update npr_topic_modeling.R

--- npr_topic_modeling.R
+++ npr_topic_modeling.R
@@ -1,8 +1,11 @@
 # topic modeling
 
-topics <- LDA(DocumentTermMatrix(VCorpus(VectorSource(articlez[1:legit_articles]))),3)
+topics <- LDA(DocumentTermMatrix(VCorpus(VectorSource(articlez[1:legit_articles]))),5)
 
 get_terms(topics,10)
 
 plot(posterior(topics)$topics[,2]~as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"),pch=20)
 points(posterior(topics)$topics[,3]~as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"),pch=20,col="gray")
+
+
+plot(aggregate(posterior(topics)$topics[,2]~format.Date(as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"), "%U"),FUN=function(x){return(length(which(x >= .5)))}),type="l")
