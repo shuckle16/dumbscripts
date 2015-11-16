cae5cac8b56c6304016eda60c51029f127873f14
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-15 22:58:26 -0500

    cleaner

--- polling_data.R
+++ polling_data.R
@@ -1,5 +1,6 @@
 library(stringr)
 library(dplyr)
+
 polls <- read.csv("~/bzan/3/tm/project/polling_data.csv")
 polls <- polls[,c(1,2,grep(names(polls),pattern = "[Tt]rump"))]
 
@@ -7,9 +8,8 @@ polls$Trump <- as.numeric(as.character(polls$Trump))
 polls$Date <- as.Date(paste(word(polls[,2],3),"/2015",sep=""),"%m/%d/%Y")
 polls$week <- format.Date(polls$Date,"%U")
 
-tempdf$week <- format.Date(tempdf$X2,"%U")
 
-weekly_counts <- aggregate(tempdf$trump_in_title~tempdf$week,FUN=sum)
+weekly_counts <- aggregate(title_df$trump_in_title~title_df$week,FUN=sum)
 names(weekly_counts) <- c("week","trump_in_title")
 
 weekly_polls <- aggregate(polls$Trump~polls$week,FUN=median)
@@ -27,15 +27,10 @@ lines(lowess(weekly_polls),lwd=3,col="black")
 lines(lowess(weekly_counts),lwd=3,col="blue")
 
 
-newdf <- inner_join(weekly_polls,weekly_counts)
+trump <- inner_join(weekly_polls,weekly_counts)
+trump <- inner_join(trump_mentions,trump)
 
-cor(newdf$trump_poll,newdf$trump_in_title)
-
-#install.packages("MSBVAR")
+# granger tests 
 library(MSBVAR)
-granger.test(newdf[2:3],1)
-
-
-trump <- inner_join(trump_mentions,newdf)
-
-granger.test(trump[,2:3],1)
+granger.test(trump[2:3],1)
+granger.test(trump[,3:4],1)
