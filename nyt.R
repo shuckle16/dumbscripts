8b03092431c5e62a4419f1e21cf8762951f10cdf
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-16 22:18:12 -0500

    continuity of articles

--- nyt.R
+++ nyt.R
@@ -10,10 +10,10 @@ titles <- character(1000)
 
 # loop through 100 pages from june 15
 for (i in 1:100) {
-
+  
   uri <- paste("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=donald+trump&begin_date=20150615&sort=oldest&page=",
                i,"&api-key=7f93af337b0212c846592746b610282e:3:73217497",sep="")
-
+  
   page_json <- read_html(uri) %>%
     html_text %>%
     fromJSON()
@@ -26,7 +26,7 @@ for (i in 1:100) {
   }
 }
 
-mydf <- as.data.frame(cbind(dts,titles,urls))
+mydf <- data.frame(dts,titles,urls)
 
 iterator <- 0
 dts <- character(1000)
@@ -52,18 +52,48 @@ for (i in 1:100) {
   }
 }
 
-mydf2 <- as.data.frame(cbind(dts,titles,urls))
-
-mydf3 <- rbind(mydf,mydf2)
-mydf3$trump_in_title <- grepl("[Tt]rump",mydf3$titles)
-mydf3$dts <- as.Date(substr(mydf3$dts,1,10))
+mydf2 <- data.frame(dts,titles,urls)
 
 
-# note did not get enough stuff from august 25 - 27
+iterator <- 0
+dts <- character(1000)
+urls <- character(1000)
+titles <- character(1000)
+
+for (i in 1:100) {
+  
+  uri <- paste("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=donald+trump&begin_date=20150825&sort=oldest&page=",
+               i,"&api-key=7f93af337b0212c846592746b610282e:3:73217497",sep="")
+  
+  page_json <- read_html(uri) %>%
+    html_text %>%
+    fromJSON()
+  
+  for (j in 1:(length(page_json$response$docs))) {
+    iterator <- iterator + 1
+    dts[iterator] <- page_json$response$docs[[j]]$pub_date
+    urls[iterator] <- page_json$response$docs[[j]]$web_url
+    titles[iterator] <- page_json$response$docs[[j]]$headline$main
+  }
+}
+
+mydf4 <- data.frame(dts,titles,urls)
+full_nyt <- rbind(mydf,mydf2,mydf4)
+
+full_nyt$trump_in_title <- grepl("[Tt]rump",full_nyt$titles)
+full_nyt$dts <- as.Date(substr(full_nyt$dts,1,10))
+
+full_nyt <- unique(full_nyt)
+
 
 # filter out videos
-mydf3 <- mydf3[-grep("video",mydf$urls),]
+full_nyt <- full_nyt[-grep("video",full_nyt$urls),]
 
+
+plot(aggregate(full_nyt$trump_in_title~format(full_nyt$dts,"%U"),FUN=sum),type="l")
+lines(lowess(aggregate(full_nyt$trump_in_title~format(full_nyt$dts,"%U"),FUN=sum)),col="blue",lwd=2)
+
+lines(trump$trump_poll~trump$week)
 
 # getting the text 
 # doesn't work yet
