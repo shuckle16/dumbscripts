cb43f470fa6bd2e7dae533f511cd5d0a8c59c31d
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-22 10:20:04 -0500

    Update npr_make.R

--- npr_make.R
+++ npr_make.R
@@ -1,3 +1,17 @@
-source("npr.R")
-source("polling_data.R")
-source("clean_npr_articles.R")
+# republicans as of 11/22/2015
+
+republicans <- c("donald trump", "ben carson", "carly fiorina",
+                 "marco rubio", "jeb bush", "ted cruz",
+                 "rand paul", "john kasich", "mike huckabee",
+                 "chris christie", "rick santorum", "lindsey graham", "george pataki")
+
+
+for (candidate in republicans) {
+  source("npr.R")
+  source("polling_data.R")
+  source("clean_npr_articles.R")
+  
+  # to do
+  # save p-values from granger test to a dataframe
+  # save sentiment scores of articles from some date to now
+}
