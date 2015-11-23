d87476a138570951947269ad9848645764883991
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-23 11:25:20 -0500

    Delete twitter_pics.py

--- twitter_pics.py
+++
@@ -1,8 +0,0 @@
-from BeautifulSoup import BeautifulSoup
-import urllib2
-url = "https://www.twitter.com/TWITTERHANDLE/media"
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
-pics = soup.findAll('img',{'class':'TwitterPhoto-mediaSource'})
-for pic in pics:
-	print pic["src"]
