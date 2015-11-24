a9265a788408db065c250485c32e3702bdc15adb
shuckle16 <shuckle16@users.noreply.github.com>
2015-11-24 08:39:17 -0500

    Update npr_make.R

--- npr_make.R
+++ npr_make.R
@@ -6,13 +6,28 @@ republicans <- c("donald trump", "ben carson", "carly fiorina",
                  "chris christie", "rick santorum", "lindsey graham", "george pataki")
 
 
+republican_df <- data.frame(name                   =character(),
+                            mentions_implies_polls =numeric(),
+                            polls_implies_mentions =numeric(),
+                            total_mentions         =numeric(),
+                            mentions_in_titles     =numeric())
+
 for (candidate in republicans) {
   source("npr.R")
   source("polling_data.R")
   source("clean_npr_articles.R")
   source("sentiment_analysis_stuff.R")
   
-  # to do
+  # to do 
   # save p-values from granger test to a dataframe
   # save sentiment scores of articles from some date to now
+  
+  new_row <- data.frame(name                   = candidate,
+                        mentions_implies_polls = mip,
+                        polls_implies_mentions = pim,
+                        total_mentions         = t_m,
+                        mentions_in_titles     = mit)
+  
+  republican_df <- rbind(republican_df,new_row)
+  
 }
