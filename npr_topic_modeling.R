c3310d363e8841d09835953cef3d565f052e8119
biz <shuckle16@hushmail.com>
2015-11-29 10:37:05 -0500

    topic modeling

--- npr_topic_modeling.R
+++ npr_topic_modeling.R
@@ -2,7 +2,7 @@
 
 library(topicmodels)
 
-topics <- LDA(DocumentTermMatrix(VCorpus(VectorSource(articlez))),6)
+topics <- LDA(DocumentTermMatrix(VCorpus(VectorSource(articlez))),2)
 
 get_terms(topics,10)
 
@@ -10,4 +10,21 @@ plot(posterior(topics)$topics[,2]~as.Date(word(unlist(pubdatez[1:legit_articles]
 points(posterior(topics)$topics[,3]~as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"),pch=20,col="gray")
 
 
-plot(aggregate(posterior(topics)$topics[,2]~format.Date(as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"), "%U"),FUN=function(x){return(length(which(x >= .5)))}),type="l")
+plot(aggregate(posterior(topics)$topics[,3]~format.Date(as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"), "%U"),FUN=function(x){return(length(which(x >= .5)))}),type="l")
+
+topics_df <- aggregate(posterior(topics)$topics[,1]~format.Date(as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"), "%U"),FUN=function(x){return(length(which(x >= .5)))})
+names(topics_df) <- c("week","topic_count")
+
+topics_df2 <- aggregate(posterior(topics)$topics[,2]~format.Date(as.Date(word(unlist(pubdatez[1:legit_articles]),2,4),"%d %b %Y"), "%U"),FUN=function(x){return(length(which(x >= .5)))})
+names(topics_df2) <- c("week","topic_count2")
+
+topics_df <- inner_join(topics_df,topics_df2)
+
+candidate_df <- inner_join(candidate_df,topics_df)
+
+candidate_df$topic_count3 <- candidate_df$topic_count + candidate_df$topic_count2
+
+plot(candidate_df$candidate_poll~candidate_df$week,type="o",ylim=c(0,40))
+lines(candidate_df$topic_count~candidate_df$week,type="o")
+
+granger.test(candidate_df[,c(3,7)],1)
