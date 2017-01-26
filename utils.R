a243349c0ed66b1686964860c302d1be5023d2d6
shuckle16 <shuckle16@users.noreply.github.com>
2017-01-25 19:46:28 -0500

    Update utils.R

--- utils.R
+++ utils.R
@@ -2,6 +2,14 @@ first_of_month <- function(dt) {
   as.Date(paste(month(dt),"01",year(dt)),"%m %d %Y")
 }
 
-week_number <- function(dt,start_year=2015) {
-  week(dt) + 52*(year(dt) - start_year)
+week_number <- function(dt,start_year=NULL) {
+  if(is.null(start_year)) {
+    week(dt) + 52*(year(dt) - year(min(dt)))
+  } else {
+    week(dt) + 52*(year(dt) - start_year)
+  }
+}
+
+week_starting_with <- function(dt) {
+  min(dt) + 7*week_number(dt)
 }
